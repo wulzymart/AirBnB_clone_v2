94af5eda4627e759283255f299dd0cb9a6ab87c3
Olawole Olawale Martins <olawalemart@gmail.com>
2023-10-04 21:18:09 +0100

    handling failes in task 2

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -3,49 +3,34 @@
 
 
 from fabric.api import *
-from time import strftime
-from datetime import date
+from os import path
 
 env.hosts = ['100.26.255.19', '54.237.2.219']
 env.user = 'ubuntu'
 env.key_filename = '~/.ssh/school'
 
 
-def do_pack():
-    """generates tar gz from web-static"""
-    file_name = f"web_static_{strftime('%Y%m%d%H%M%S')}.tgz"
-    try:
-        local("mkdir -p versions")
-        res = local(f"tar -czvf versions/{file_name} web_static/")
-
-        return f"versions/{file_name}" if res.succeeded else None
-
-    except Exception as e:
-        return None
-
-
 def do_deploy(archive_path):
     """distributes an archive to your web servers"""
-    from os import path
 
-    if not archive_path:
+    if not archive_path or not path.exists(archive_path)\
+            or not path.isfile(archive_path):
         return False
 
-    name_with_no_ext = archive_path.split('.')[0].split("/")[1]
-    try:
-        if not path.exists(archive_path):
-            return False
-        put(local_path=archive_path, remote_path="/tmp/")
-        run(f"mkdir -p /data/web_static/releases/{name_with_no_ext}")
+    name_with_no_ext = path.basename(archive_path).split('.')[0]
+
+    if put(local_path=archive_path, remote_path="/tmp/").failed or\
+        run(f"mkdir -p /data/web_static/releases/{\
+            name_with_no_ext}").failed or\
         run(f"tar -xzf /tmp/{name_with_no_ext}.tgz -C \
- /data/web_static/releases/{name_with_no_ext}/")
-        run(f"rm /tmp/{name_with_no_ext}.tgz")
+ /data/web_static/releases/{name_with_no_ext}/").failed or\
+        run(f"rm /tmp/{name_with_no_ext}.tgz").failed or\
         run(f"mv /data/web_static/releases/{name_with_no_ext}/web_static/*\
- /data/web_static/releases/{name_with_no_ext}/")
-        run(f"rm -rf /data/web_static/releases/{name_with_no_ext}/web_static")
-        run(f"unlink /data/web_static/current")
+ /data/web_static/releases/{name_with_no_ext}/").failed or\
+        run(f"rm -rf /data/web_static/releases/{\
+            name_with_no_ext}/web_static").failed or\
+        run(f"rm -rf /data/web_static/current").failed or\
         run(f"ln -s /data/web_static/releases/{name_with_no_ext}\
- /data/web_static/current")
-        return True
-    except Exception as e:
+ /data/web_static/current").failed:
         return False
+    return True
